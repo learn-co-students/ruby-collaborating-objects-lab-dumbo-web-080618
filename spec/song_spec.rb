5a45f161dec93cf6b94c7a32ff97422f07ae185c
github username <github email address>
2018-08-10 19:13:56 -0400

    finished

--- spec/song_spec.rb
+++ spec/song_spec.rb
@@ -31,7 +31,6 @@ describe 'Song' do
 
     it 'associates new song instance with the artist from the filename' do
       Artist.class_variable_set("@@all",[])
-      # binding.pry
       new_instance = Song.new_by_filename(file_name)
       expect(new_instance.artist.name).to eq('Michael Jackson')
       expect(Artist.all.size).to eq(1)
